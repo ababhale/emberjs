06c069969f2c069dcb22af54c639d1bab4bf1e75
unknown <ababhale@ABABHALE-M83.na.nice.com>
2015-09-03 15:08:50 +0530

    Modifications for router demo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 App = Ember.Application.create();
 
+App.Person = DS.Model.extend({
+	firstName : DS.attr(),
+	lastName : DS.attr(),
+	email : DS.attr()  
+});
+
+App.PersonAdapter = DS.FixtureAdapter.extend({
+});
+
+App.Person.FIXTURES = [{"id" : 1, "firstName":"Ashish","lastName":"Babhale","email":"ashish.babhale@nice.com"}];
+
 App.Router.map(function() {
 	this.route("first", {
 		path : "first"
@@ -13,7 +24,7 @@ App.Router.map(function() {
 	});
 
 	this.route("edit-customer", {
-		path :"edit-customer"
+		path :"edit-customer/:id"
 	});
 
 	this.route("add-customer", {
@@ -37,7 +48,8 @@ App.FirstRoute = Ember.Route.extend({
 App.CustomerRoute = Ember.Route.extend({
 	model : function(){
 		var customers = JSON.parse(localStorage.getItem("customers"));
-		return customers;
+		//return customers;
+		return this.store.findAll('person');
 	}
 }); 
 
@@ -55,6 +67,17 @@ App.CustomerController = Ember.ArrayController.extend({
 			console.log(this.model);
 			console.log(this);
 			//this.transitionToRoute("edit-customer");
+		}
+	}
+});
+
+App.EditCustomerController = Ember.Controller.extend({
+	actions : {
+		saveCustomer : function () {
+			var customerModel = this.get('model');
+			alert(customerModel.get("firstName"))
+			alert(this.model.get("firstName"))
+			alert('Save customer called');
 		}
 	}
 });
